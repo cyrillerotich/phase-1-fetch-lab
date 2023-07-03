e014cc92e98109180184e593ed3cefeb92303fbb
cyrille <cyrilllerotich@gmail.com>
2023-07-03 12:44:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+
+// renderBooks()
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
   
-}
+// }
+function fetchBooks(){
+  fetch("https://anapioficeandfire.com/api/books")
+.then( function(response){
+  return response.json(); 
+})
+.then(function(data){
+  console.log(data);
+  renderBooks(data);
+})
+
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerText= book.name;
     main.appendChild(h2);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+ fetchBooks()
 });
+
